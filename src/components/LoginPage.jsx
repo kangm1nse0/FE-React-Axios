d5f8370051c996d323ccad1beade82af1b945953
kangm1nse0 <alstj2004102@naver.com>
2025-06-03 17:12:50 +0900

    과제최종제출

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,57 +1,42 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import "../styles/LoginPage.css";
+ import React, { useState } from "react";
+  import axios from "axios";
+  import { useNavigate } from "react-router-dom";
+  import "../styles/LoginPage.css";
 
-function LoginForm() {
-   const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
-  const [message,setMessage]=useState(""); 
-  const navigate = useNavigate(); 
+  function LoginForm() {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const navigate = useNavigate();
 
+    const handleLogin = async (e) => {
+      e.preventDefault();
+      try{
+        const data = await axios.post(
+          "https://reqres.in/api/login",{
+          email,password},
+          {
+          headers: {
+            "x-api-key": "reqres-free-v1"
+          }
+        }
+      );
+      localStorage.setItem("email",email);
+      localStorage.setItem("token",data.data.token);
 
-  const postUser = async () => {
-    try {
-    const res  = await axios.post("https://reqres.in/api/login", {
-      email : email,
-      password : password,
-    }, {
-      headers: {
-        "x-api-key": "reqres-free-v1"} 
-     });
-
-    localStorage.setItem("token", res.data.token); 
-    localStorage.setItem("email", email); 
-    navigate("/userlist"); 
-    
-  } catch (error) { 
-    const errMsg = error.response?.data?.error || "User not found";
-      setMessage(errMsg); 
+      navigate("/UserList");
+      }catch(e){
+        const em = e.response?.data?.error
+        console.log(em);
+      }
+    };
+    return (
+      <form onSubmit={handleLogin} className="login-container">
+        <h2>🔐 로그인</h2>
+        <input type="email" placeholder="이메일"  value={email} required onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="비밀번호" value={password} required onChange={(e)=>setPassword(e.target.value)}/>
+        <button type="submit">로그인</button>
+      </form>
+    );
   }
-  };
 
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    await postUser(); 
-};
- const handleEmail = (e) => {
-    setEmail(e.target.value)
-  };
-
-  const handlePW = (e) => {
-    setPassword(e.target.value)
-  };
-
-  return (
-   <form onSubmit={handleLogin} className="login-container">
-      <h2>🔐 로그인</h2>
-      <input required value ={email} onChange={handleEmail} placeholder="이메일 입력"/>
-      <input required value={password} onChange={handlePW} placeholder="비밀번호 입력" type="password"/>
-      <button type="submit" >로그인</button>
-      {message && <p className="message">{message}</p>}
-    </form>
-  );
-}
-
-export default LoginForm;
+  export default LoginForm;
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,64 +2,59 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/UserList.css";
 
-function UserCard({ user }) {
-  return (
-    <div className="user-card" key={user.id}>
-      <img className="user-avatar" src={user.avatar} alt="avatar" />
-      <h4>{user.first_name} {user.last_name}</h4>
-      <p className="user-email">{user.email}</p>
-    </div>
-  );
-} 
 function UserList() {
-const [users, setUsers] = useState([]);
-  const [email, setEmail] = useState("");
-  const [token, setToken] = useState("");
-  const [search, setSearch] = useState("");
+  const [users,setUsers] = useState([]);
+  const [keyword,setKeyword] = useState("");
 
-  useEffect(() => {
-    // 로그인한 사용자 정보 불러오기
-    const storedEmail = localStorage.getItem("email");
-    const storedToken = localStorage.getItem("token");
+  useEffect(()=>{
 
-    if (storedEmail) setEmail(storedEmail);
-    if (storedToken) setToken(storedToken);
+   const fetch = async ()=>{
+      try{
+        const ag = await axios.get(
+          "https://reqres.in/api/users?page=2",
+          {
+            headers:{
+              "x-api-key": "reqres-free-v1"
+            }
+          }  
+        );
+        setUsers(ag.data.data);
+      }catch(e){
+        console.log(e);
+      }
+    }
+    fetch();
+  },[]);  
 
-    // 사용자 목록 요청
-    axios.get("https://reqres.in/api/users")
-      .then((res) => {
-        setUsers(res.data.data);
-      })
-      .catch((err) => {
-        console.error("유저 불러오기 실패:", err);
-      });
-  }, []);
-
-  // 검색 필터링
-  const filteredUsers = users.filter((user) =>
-    user.email.toLowerCase().includes(search.toLowerCase())
-  );
+  const search = users.filter((u)=>{
+    const info = `${u.first_name}${u.last_name}<br>${u.email}`;
+    return info.includes(keyword);
+  })
   return (
     <div className="user-container">
       <div className="user-info-box">
-        <p><strong>이메일:</strong> { email}</p>
-        <p><strong>토큰:</strong> {token }</p>
+        <p><strong>이메일:</strong>{localStorage.getItem('email')}</p>
+        <p><strong>토큰:</strong> {localStorage.getItem('token')}</p>
       </div>
 
       <h2 className="user-title">👥 유저 목록</h2>
 
       <input
         className="user-search"
-        placeholder="이메일 검색"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        placeholder="이름 또는 이메일로 검색하세요"
+        value={keyword}
+        onChange={(e)=>{setKeyword(e.target.value)}}
       />
 
-     <div className="userlist">
-          {filteredUsers.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-        </div>
+      <div className="user-list">
+        {search.length?search.map((u)=>(
+          <div className="user-card">
+          <img className="user-avatar" src={u.avatar} alt="img"/>
+          <p>{u.first_name} {u.last_name}</p> 
+          <p className="user-email">{u.email}</p> 
+          </div>
+        )):<div>검색 결과가 없습니다.</div>}        
+      </div>
     </div>
   );
 }
